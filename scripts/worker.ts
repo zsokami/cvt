4506b0c9c8de9132f4b013575a34a51a46584402
钟孟捷 <718335940@qq.com>
2025-01-25 03:40:15 +0800

    Worker

---
+++ scripts/worker.ts
@@ -0,0 +1,3 @@
+import fetch from '../netlify/edge-functions/main/main.ts'
+
+export default { fetch }
---
+++ scripts/worker_build.ts
@@ -0,0 +1,27 @@
+import * as esbuild from 'https://deno.land/x/esbuild@v0.24.2/mod.js'
+
+await esbuild.build({
+  entryPoints: ['scripts/worker.ts'],
+  outfile: 'scripts/__worker.js',
+  bundle: true,
+  format: 'esm',
+  charset: 'utf8',
+  plugins: [{
+    name: 'http',
+    setup(build) {
+      build.onResolve({ filter: /^https?:\/\// }, ({ path }) => ({
+        path,
+        namespace: 'http',
+      }))
+      build.onResolve({ filter: /.*/, namespace: 'http' }, ({ path, importer }) => ({
+        path: new URL(path, importer).toString(),
+        namespace: 'http',
+      }))
+      build.onLoad({ filter: /.*/, namespace: 'http' }, async ({ path }) => {
+        const { code } = await esbuild.transform(await (await fetch(path)).text(), { loader: 'ts' })
+        return { contents: code }
+      })
+    },
+  }],
+})
+await esbuild.stop()
