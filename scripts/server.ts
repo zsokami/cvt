b25559e0974cfd494e755ba7891a5cea1b13f7cc
钟孟捷 <718335940@qq.com>
2025-02-20 22:19:21 +0800

    支持环境变量 HOST IP PORT

--- scripts/server.ts
+++ scripts/server.ts
@@ -1,7 +1,8 @@
 import handler from '../netlify/edge-functions/main/main.ts'
 import { splitRight } from '../netlify/edge-functions/main/utils.ts'
 
-let hostname, port
+let hostname = Deno.env.get('HOST') || Deno.env.get('IP')
+let port = Number(Deno.env.get('PORT')) || undefined
 
 if (Deno.args[0]) {
   const [a, b] = splitRight(Deno.args[0], ':')
